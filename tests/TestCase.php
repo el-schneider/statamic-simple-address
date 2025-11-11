2b52ceea1847cb919eba6cbd1355d7043acf1079
el-schneider <mail@jonaslist.de>
2025-11-11 20:01:41 +0100

    feat: add PreventsSavingStacheItemsToDisk trait

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,12 @@ namespace Tests;
 
 use ElSchneider\StatamicSimpleAddress\ServiceProvider;
 use Statamic\Testing\AddonTestCase;
+use Statamic\Testing\Concerns\PreventsSavingStacheItemsToDisk;
 
 abstract class TestCase extends AddonTestCase
 {
+    use PreventsSavingStacheItemsToDisk;
+
     protected string $addonName = 'el-schneider/statamic-simple-address';
 
     protected string $addonServiceProvider = ServiceProvider::class;
@@ -16,7 +19,6 @@ abstract class TestCase extends AddonTestCase
         parent::resolveApplicationConfiguration($app);
 
         $app['config']['statamic.editions.pro'] = true;
-        $app['config']['app.key'] = 'base64:'.base64_encode(random_bytes(32));
 
         // Load the addon config
         $app['config']->set('simple-address', require __DIR__.'/../config/simple-address.php');
